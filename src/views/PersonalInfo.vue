d94d6e8d3794fe6cc30b0f8e76b9bf5d32003452
galaxyuliana <yulianashin613@gmail.com>
2022-05-28 03:52:57 +0900

    Added PersonalInfo page

---
+++ src/views/PersonalInfo.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <div class="personal-info">
+      <h1 id="name">Yuliana Shin</h1>
+      <h2 id="position">Full Stack Web Developer</h2>
+      <ul id="details">
+        <li>Master of Engineering (Computer Engineering)</li>
+        <li>
+          Experienced in IT solution, Insurance Broker, and Media Industry
+        </li>
+      </ul>
+      <ul id="contacts">
+        <li>
+          <a href="mailto:yulianashin613@gmail.com">yulianashin613@gmail.com</a>
+        </li>
+        <li>
+          <a href="https://github.com/galaxyuliana">https://github.com/galaxyuliana</a>
+        </li>
+      </ul>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+#name {
+  font-weight: bold;
+}
+#position {
+  margin: 10px 0px;
+}
+#details {
+  padding-inline-start: 20px;
+}
+#contacts {
+  list-style: none;
+  padding: 0px;
+}
+</style>
