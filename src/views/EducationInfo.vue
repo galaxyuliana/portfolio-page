870535a9f8217cff643e3b55555cebafdaaabe87
galaxyuliana <yulianashin613@gmail.com>
2022-05-29 17:37:15 +0900

    Updated overall layout

--- src/views/EducationInfo.vue
+++ src/views/EducationInfo.vue
@@ -10,7 +10,7 @@
         <h4>Seoul, Republic of Korea</h4>
         <h4>Computer Engineering</h4>
         <h4>Master in Engineering</h4>
-        <h4>GPA 4.28 / 4.3 | Honor Student</h4>
+        <h4>GPA 4.28 / 4.3 | Graduated as a honor student</h4>
         <h4>Mar. 2019 ~ Aug. 2021</h4>
         <h4>Went to Yonsei University Graduate School of Engineering (GSE). Algorithm, Android Application, SQL, Python, AI</h4>
       </div>
@@ -22,7 +22,7 @@
         <h4>Seoul, Republic of Korea</h4>
         <h4>Smart IT</h4>
         <h4>Bachelor of Science</h4>
-        <h4>GPA 4.5 / 4.5 | Honor Student</h4>
+        <h4>GPA 4.5 / 4.5 | Graduated as a honor student</h4>
         <h4>Mar. 2017 ~ Feb. 2018</h4>
         <h4>PHP, Design</h4>
       </div>
@@ -34,7 +34,7 @@
         <h4>Seoul, Republic of Korea</h4>
         <h4>Smart IT</h4>
         <h4>Associate Degree of Science</h4>
-        <h4>GPA 4.47 / 4.5 | Honor Student</h4>
+        <h4>GPA 4.47 / 4.5 | Graduated as a honor student</h4>
         <h4>Mar. 2014 ~ Feb. 2017</h4>
         <h4>Java, .NET, C#, HTML, JQuery, MS-SQL, Data Structure</h4>
       </div>
--- src/views/ProfileInfo.vue
+++ src/views/ProfileInfo.vue
@@ -1,13 +1,15 @@
 <template>
-  <main>
+  <div class="profile">
     <div class="personal-info">
-      <h1 id="name">Yuliana Shin</h1>
+      <h1 id="name">Yuliana Y. Shin</h1>
       <h2 id="position">Full Stack Web Developer</h2>
       <ul id="details">
-        <li>Master of Engineering (Computer Engineering)</li>
+        <li>4+ years experience in Web Development</li>
         <li>
           Experienced in IT solution, Insurance Broker, and Media Industry
         </li>
+        <li>Master of Engineering (Computer Engineering)</li>
+        <li>Arriving in Vancouver on June 23rd</li>
       </ul>
       <ul id="contacts">
         <li>
@@ -18,7 +20,15 @@
         </li>
       </ul>
     </div>
-  </main>
+    <div class="about-me">
+        Hello, My name is Yuliana Y. Shin. I used to work as Full Stack Web Developer in Korea.
+    </div>
+    <div class="skills">
+        Skill Sets
+        <br/>
+        years of used excluding academic years
+    </div>
+  </div>
 </template>
 
 <style scoped>
