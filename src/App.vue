1fa63ca77a4b5896bc0bb26e87fcac46742427f6
galaxyuliana <yulianashin613@gmail.com>
2022-06-25 14:08:30 -0700

    Added fontawesome

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,19 @@ import { RouterLink, RouterView } from "vue-router";
   <main>
     <RouterView />
   </main>
+  <footer>
+    <div class="wrapper">
+      <a href="mailto:yulianashin613@gmail.com">
+        <font-awesome-icon :icon="['far', 'envelope']" />
+      </a>
+      <a href="https://github.com/galaxyuliana">
+        <font-awesome-icon :icon="['fab', 'github']" />
+      </a>
+      <a href="https://github.com/galaxyuliana/portfolio-page">
+        <font-awesome-icon :icon="['far', 'file-code']" />
+      </a>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faFileCode, faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faGithub, faFileCode, faEnvelope);
 
 const app = createApp(App);
-
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router);
 
 app.mount("#app");
